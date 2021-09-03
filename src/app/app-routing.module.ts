7ebb9129b7c9121b023546ede7b04c60a844e70d
maihesham <maihesham19@yahoo.com>
2021-09-03 09:29:44 -0700

    add All Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './add/add.component';
+import { ListComponent } from './list/list.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UpdateComponent } from './update/update.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"Add",component:AddComponent
+  },{
+    path:"List",component:ListComponent
+  },{
+    path:"Update",component:UpdateComponent
+  },{
+    path:"Login",component:LoginComponent
+  },{
+    path:"Register",component:RegisterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
